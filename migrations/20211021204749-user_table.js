9a6cbf050c3a0aed03f81f9a73d9b99a0aec1a1a
Doruk Kutlu <me@doruk.link>
2021-11-18 00:27:04 +0300

    define unique index in define object

--- migrations/20211021204749-user_table.js
+++ migrations/20211021204749-user_table.js
@@ -1,37 +1,26 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable(
-      "users",
-      {
-        id: {
-          type: Sequelize.INTEGER,
-          primaryKey: true,
-          autoIncrement: true,
-        },
-        name: Sequelize.STRING,
-        email: Sequelize.STRING,
-        password: Sequelize.STRING,
-        created_at: {
-          type: Sequelize.DATE(3),
-          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP(3)"),
-        },
-        updated_at: {
-          type: Sequelize.DATE(3),
-          defaultValue: Sequelize.literal(
-            "CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)"
-          ),
-        },
-        deleted_at: Sequelize.DATE(3),
+    await queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
       },
-      {
-        indexes: [
-          {
-            unique: true,
-            fields: ["email"],
-          },
-        ],
-      }
-    );
+      name: Sequelize.STRING,
+      email: { type: Sequelize.STRING, unique: true },
+      password: Sequelize.STRING,
+      created_at: {
+        type: Sequelize.DATE(3),
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP(3)"),
+      },
+      updated_at: {
+        type: Sequelize.DATE(3),
+        defaultValue: Sequelize.literal(
+          "CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)"
+        ),
+      },
+      deleted_at: Sequelize.DATE(3),
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
--- migrations/20211112011942-tags_table.js.js
+++ migrations/20211112011942-tags_table.js.js
@@ -1,35 +1,24 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable(
-      "tags",
-      {
-        id: {
-          primaryKey: true,
-          type: Sequelize.INTEGER,
-          autoIncrement: true,
-        },
-        name: Sequelize.STRING,
-        created_at: {
-          type: Sequelize.DATE(3),
-          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP(3)"),
-        },
-        updated_at: {
-          type: Sequelize.DATE(3),
-          defaultValue: Sequelize.literal(
-            "CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)"
-          ),
-        },
-        deleted_at: Sequelize.DATE(3),
+    await queryInterface.createTable("tags", {
+      id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
       },
-      {
-        indexes: [
-          {
-            unique: true,
-            fields: ["name"],
-          },
-        ],
-      }
-    );
+      name: { type: Sequelize.STRING, unique: true },
+      created_at: {
+        type: Sequelize.DATE(3),
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP(3)"),
+      },
+      updated_at: {
+        type: Sequelize.DATE(3),
+        defaultValue: Sequelize.literal(
+          "CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)"
+        ),
+      },
+      deleted_at: Sequelize.DATE(3),
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
