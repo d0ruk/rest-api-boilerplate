9813a79663469f04317f48338f497d8e5836de5e
Doruk Kutlu <me@doruk.link>
2021-11-18 00:29:02 +0300

    seed admin user

--- seeders/20211021205046-users.js
+++ seeders/20211021205046-users.js
@@ -1,22 +1,34 @@
 const faker = require("faker");
+const config = require("config");
 
 const { hashWithSalt } = require("../dist/util/");
 
+const { name, password: _password, email } = config.get("app.admin");
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const [hash, salt] = await hashWithSalt("12345");
+    const [hash, salt] = await hashWithSalt(_password);
 
     await queryInterface.bulkInsert(
       "users",
-      Array.from({ length: 100 }, () => ({
-        name: faker.name.findName(),
-        email: faker.internet.email(),
-        password: hash.toString("hex"),
-        salt,
-        created_at: new Date(),
-        updated_at: new Date(),
-      })),
-      {}
+      [
+        {
+          name,
+          email,
+          password: hash.toString("hex"),
+          salt,
+          role: 0,
+        },
+      ].concat(
+        Array.from({ length: 99 }, () => ({
+          name: faker.name.findName(),
+          email: faker.internet.email(),
+          password: hash.toString("hex"),
+          salt,
+          created_at: new Date(),
+          updated_at: new Date(),
+        }))
+      )
     );
   },
 
